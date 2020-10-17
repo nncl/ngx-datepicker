7641a643c652105e8b0ee69a64723b1d5d66b599
Caue Almeida <nncl@live.com>
2020-10-17 14:22:51 -0300

    feat(example): Added example on how to work with ngModel
    Closes #9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { NgxdatepickerModule } from "ngxdatepicker";
+import { NgxdatepickerModule } from "projects/ngxdatepicker/src/public-api";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxdatepickerModule],
+  imports: [BrowserModule, NgxdatepickerModule, FormsModule],
   providers: [],
   exports: [],
   bootstrap: [AppComponent],
