76c0c23c19c9da4ba9f8e130065cf55e6195eb26
Caue <nncl@live.com>
2021-08-01 14:41:39 -0300

    style: Applying prettier

--- projects/ngxdatepicker/src/lib/ngxdatepicker.module.ts
+++ projects/ngxdatepicker/src/lib/ngxdatepicker.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from "@angular/core";
-import { NgxdatepickerComponent } from "./ngxdatepicker.component";
-import { CommonModule } from "@angular/common";
-import { WeekdayPipe } from "../pipes/weekday/weekday.pipe";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { NgxdatepickerComponent } from './ngxdatepicker.component';
+import { CommonModule } from '@angular/common';
+import { WeekdayPipe } from '../pipes/weekday/weekday.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [NgxdatepickerComponent, WeekdayPipe],
-  imports: [CommonModule, FormsModule],
-  exports: [NgxdatepickerComponent, WeekdayPipe],
+  declarations: [ NgxdatepickerComponent, WeekdayPipe ],
+  imports: [ CommonModule, FormsModule ],
+  exports: [ NgxdatepickerComponent, WeekdayPipe ],
 })
-export class NgxdatepickerModule {}
+export class NgxdatepickerModule {
+}
